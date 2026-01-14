afd04034513d2735ffb848bd7f1b2cb35872e539
Sadeepa11 <srisadeepa658@gmail.com>
2026-01-14 19:25:24 +0530

    update data base

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,12 +1,21 @@
 const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+require('dotenv').config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'modulink-uploads',
+        allowed_formats: ['jpg', 'png', 'jpeg', 'gif'],
     },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
 });
 
 const upload = multer({ storage: storage });
