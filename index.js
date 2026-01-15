9235af4d90825e1d28b3cf4f29b5c19a2f2e402e
Sadeepa11 <srisadeepa658@gmail.com>
2026-01-15 12:03:25 +0530

    Update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // Silence favicon requests
 app.get('/favicon.ico', (req, res) => res.status(204).end());
